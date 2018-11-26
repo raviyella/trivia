ec41bc1c84faf45bb0eac6b36f8dc3bf1d5abec3
Ravi <12459058+raviyella@users.noreply.github.com>
2018-11-26 19:49:18 +0100

    adding test cases for howManyPlayers, isPlayable and add methods

--- nodejs/src/game.spec.js
+++ nodejs/src/game.spec.js
@@ -12,9 +12,37 @@ describe("The test environment", function () {
 });
 
 describe("Trivia game functionality", function () {
-  it("Game instance is created", function () {
+  it("Game instance is created and count number of players", function () {
     let game = new Game();
     Should(game).should.be.type('object');
     Should(game.howManyPlayers()).be.equal(0);
+  });
+
+  it("Players count when one or more players are added", function () {
+    let game = new Game();
+    game.add('foo');
+    Should(game.howManyPlayers()).be.equal(1);
+  });
+
+  it("Game playable when minimum players 2 are added", function () {
+    let game = new Game();
+    game.add('foo');
+    game.add('bar');
+    game.isPlayable().should.be.true();
+  });
+
+  it("Game not playable when minimum players are not added", function () {
+    let game = new Game();
+    game.add('foo');
+    game.isPlayable().should.not.be.true();
+  });
+
+  it("Add new players", function () {
+    let game = new Game();
+    game.add('foo');
+    game.add('bar');
+    game.howManyPlayers().should.be.equal(2);
+    game.places[0].should.be.equal(0);
+    game.places[1].should.be.equal(0);
   });
 });
