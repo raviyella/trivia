3d7b16c5248657e9ac864e15c10dc6f3c8aebfc7
Ravi <12459058+raviyella@users.noreply.github.com>
2018-11-27 19:15:16 +0100

    Adding GameRunner class to roll a dice

---
+++ nodejs/src/GameRunner.js
@@ -0,0 +1,19 @@
+let Game = require('./Game.js');
+
+class GameRunner {
+  constructor() {
+    let notAWinner = false;
+    let game = new Game();
+    game.add('Chet');
+    game.add('Pat');
+    game.add('Sue');
+
+    do {
+      game.roll(Math.floor(Math.random() * 6) + 1);
+
+      notAWinner = game.getNotWinner();
+    } while (notAWinner);
+  }
+}
+
+module.exports = GameRunner;
